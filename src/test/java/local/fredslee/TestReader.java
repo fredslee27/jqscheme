d3e20f2abba5793438920dbc8f1139ae95c9ede4
Fred Lee <fredslee27@gmail.com>
2015-12-20 16:30:51 -0800

    Added test for bootstrap reader.

---
+++ src/test/java/local/fredslee/TestReader.java
@@ -0,0 +1,74 @@
+package local.fredslee;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestReader
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TestReader ( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TestReader.class );
+    }
+
+    public void apply (String srctext, String expect)
+    {
+	QSreader reader0 = new QSreader();
+	reader0.parse(srctext);
+        String repr = reader0.root.toString();
+	System.out.println("src=\"" + srctext + "\" -> " + repr + " vs expect=" + expect + " => " + expect.equals(repr));
+        //System.out.println("obj=" + repr + " vs expect=" + expect + " => " + expect.equals(repr));
+        assertTrue(expect.equals(repr));
+    }
+
+    /**
+     *
+     */
+    public void testQScheme()
+    {
+        //assertTrue( true );
+	String expect = null;
+	QSobj val = null;
+
+	apply("12345", "12345");
+
+	System.out.println("----");
+
+	apply("  #t", "#t");
+
+	System.out.println("----");
+
+	apply("foobar", "foobar");
+
+	System.out.println("----");
+
+	apply("(1 2    34)", "(1 2 34)");
+
+	System.out.println("----");
+
+	apply("()", "()");
+	apply("(        )", "()");
+
+	System.out.println("----");
+
+	apply("(1    (a c d)3)", "(1 (a c d) 3)");
+    }
+}
+
